7d4c356ed155b6fb99a0b7fc080e82032db98a97
kaleb adem <kalebcode25@gmail.com>
2024-06-18 16:59:33 +0300

    v 1.2 change from node js to pure js

--- back_end/js/doc_to_html.js
+++ back_end/js/doc_to_html.js
@@ -96,21 +96,3 @@ function change_imgto_string() {
 }
 
 
-const { GoogleGenerativeAI } = require("@google/generative-ai");
-
-// Access your API key as an environment variable (see "Set up your API key" above)
-const genAI = new GoogleGenerativeAI("AIzaSyDTYPNXHwNE5nA5-uHRnBhS_mCXJSoDHXQ");
-
-async function run() {
-    // The Gemini 1.5 models are versatile and work with both text-only and multimodal prompts
-    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
-
-    const prompt = "Write a story about a magic backpack."
-
-    const result = await model.generateContent(prompt);
-    const response = await result.response;
-    const text = response.text();
-    console.log(text);
-}
-
-run();
